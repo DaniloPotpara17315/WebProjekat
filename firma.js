3b3061022f764ff333fcc2ae1a20ac852f3665d8
Danilo Potpara 17315 <danilopotpara@elfak.rs>
2021-08-02 12:35:17 +0200

    Dodata forma za dodavanje novih sektora, funkcionalnost jos nije odradjena

--- firma.js
+++ firma.js
@@ -21,35 +21,36 @@ export class Firma {
 
         elem.classList.add("izgled");
         this.container = elem;
-        elem = document.createElement("h3");
-        elem.innerHTML="Informacije firme";
+        elem = document.createElement("h2");
+        elem.innerHTML = "Informacije firme";
         elem.classList.add("colorMeBlind");
+        elem.classList.add("whiteFont");
         this.container.appendChild(elem);
         //div basic info-a
         this.crtajBasicInfo();
         //prelazimo na sektore
-        elem = document.createElement("h3");
-        elem.innerHTML="Sektori firme";
+        elem = document.createElement("h2");
+        elem.innerHTML = "Sektori firme";
         elem.classList.add("colorMeBlind");
+        elem.classList.add("whiteFont");
         this.container.appendChild(elem);
-
+        this.crtajSektore();
 
         host.appendChild(this.container);
     }
 
-    crtajBasicInfo()
-    {
+    crtajBasicInfo() {
         var elem = document.createElement("div");
         elem.classList.add("basicInfo");
 
         //levi deo prikaza basic informacija
         var leviElem = document.createElement("div");
         leviElem.classList.add("labela");
-
+        leviElem.classList.add("whiteFont");
         var nizic = ["Ime: ", "Tip: ", "Adresa: ", "Para: "];
         var elementic;
         nizic.forEach(x => {
-            elementic = document.createElement("h4");
+            elementic = document.createElement("h3");
             elementic.innerHTML = x;
             elementic.classList.add("uredi");
             leviElem.appendChild(elementic);
@@ -58,27 +59,28 @@ export class Firma {
         //desni deo prikaza basic informacija
         var desniElem = document.createElement("div");
         desniElem.classList.add("info");
+        desniElem.classList.add("whiteFont");
 
-        elementic = document.createElement("h4");
+        elementic = document.createElement("h3");
         elementic.innerHTML = this.naziv;
         elementic.classList.add("uredi");
         desniElem.appendChild(elementic);
 
-        elementic = document.createElement("h4");
+        elementic = document.createElement("h3");
         elementic.innerHTML = this.tip;
         elementic.classList.add("uredi");
         desniElem.appendChild(elementic);
 
-        elementic = document.createElement("h4");
+        elementic = document.createElement("h3");
         elementic.innerHTML = this.adresa;
         elementic.classList.add("uredi");
         desniElem.appendChild(elementic);
 
-        elementic = document.createElement("h4");
+        elementic = document.createElement("h3");
         elementic.innerHTML = this.maxBudget;
         elementic.classList.add("uredi");
         desniElem.appendChild(elementic);
-        
+
         //dodavanje levog dela
         elem.appendChild(leviElem);
         //dodavanje desnog dela
@@ -87,10 +89,117 @@ export class Firma {
         //dodavanje basic info
         this.container.appendChild(elem);
     }
-    dodajSektor(sektor){
+    dodajSektor(sektor) {
         this.sektori.push(sektor);
     }
-    crtajSektore(){
+    crtajSektore() {
+        var sektelem = document.createElement("div");
+        sektelem.classList.add("sektori");
 
+        var elem;
+        //dodavanje gotovih sektora
+        this.sektori.forEach(x => {
+
+            })
+            //dodavanje novih sektora
+        elem = document.createElement("button");
+        elem.classList.add("setButton");
+        elem.classList.add("whiteFont");
+        elem.classList.add("specialFont");
+        elem.innerHTML = "+";
+        elem.onclick = ev => {
+            sektelem.classList.add("nestani");
+            var wholeForm = document.createElement("div");
+            wholeForm.classList.add("celaForma");
+            var formZaUnos = document.createElement("div");
+            formZaUnos.classList.add("basicInfo");
+            //levi deo forme za unos
+            var redic = document.createElement("div");
+            redic.classList.add("labela");
+            var elemen = document.createElement("label");
+            elemen.innerHTML = "Unesite naziv sektora: ";
+            elemen.classList.add("whiteFont");
+            elemen.classList.add("formFont");
+            elemen.classList.add("space");
+            redic.appendChild(elemen);
+            elemen = document.createElement("label");
+            elemen.innerHTML = "Unesite zahtevani budget: ";
+            elemen.classList.add("whiteFont");
+            elemen.classList.add("formFont");
+            elemen.classList.add("space");
+            redic.appendChild(elemen);
+
+            elemen = document.createElement("label");
+            elemen.innerHTML = "Unesite kratak opis sektora: ";
+            elemen.classList.add("whiteFont");
+            elemen.classList.add("formFont");
+            elemen.classList.add("space");
+            redic.appendChild(elemen);
+            formZaUnos.appendChild(redic);
+            //kraj levog dela forme za unos
+            //pocekat desnog dela forme za unos
+            redic = document.createElement("div");
+            redic.classList.add("info");
+
+            elemen = document.createElement("input");
+            elemen.classList.add("nazivSektora");
+            elemen.classList.add("space");
+            elemen.classList.add("formUnos");
+            elemen.classList.add("whiteFont");
+            redic.appendChild(elemen);
+            elemen = document.createElement("input");
+            elemen.classList.add("budgetSektora");
+            elemen.classList.add("space");
+            elemen.type = "number";
+            elemen.classList.add("formUnos");
+            elemen.classList.add("whiteFont");
+            elemen.placeholder = "Max budget je " + this.maxBudget;
+            redic.appendChild(elemen);
+
+            elemen = document.createElement("textarea");
+            elemen.classList.add("opisSektora");
+            elemen.classList.add("space");
+            elemen.classList.add("formUnos");
+            elemen.classList.add("whiteFont");
+            elemen.rows = "4";
+            redic.appendChild(elemen);
+            formZaUnos.appendChild(redic);
+            //kraj desnog dela forme
+            wholeForm.appendChild(formZaUnos);
+            redic = document.createElement("div");
+            redic.classList.add("endForme");
+            elemen = document.createElement("button");
+            elemen.innerHTML = "Dodaj sektor";
+            elemen.classList.add("confirm");
+            elemen.onclick = ev => {
+                alert("simulacija dodato");
+                sektelem.classList.remove("nestani");
+                this.container.removeChild(wholeForm);
+            }
+            redic.appendChild(elemen);
+            elemen = document.createElement("div");
+            elemen.classList.add("spacer");
+            redic.appendChild(elemen);
+
+            elemen = document.createElement("button");
+            elemen.innerHTML = "Otkazi";
+            elemen.classList.add("deny");
+            elemen.onclick = ev => {
+                alert("Cancel");
+                sektelem.classList.remove("nestani");
+                this.container.removeChild(wholeForm);
+            }
+            redic.appendChild(elemen);
+            elemen = document.createElement("div");
+            elemen.classList.add("spacer");
+
+            wholeForm.appendChild(redic);
+            this.container.appendChild(wholeForm);
+        }
+        sektelem.appendChild(elem);
+
+
+        this.container.appendChild(sektelem);
     }
+
 }
--- sektor.js
+++ sektor.js
@@ -1,10 +1,19 @@
-export class Sektor{
-    constructor(ime,budget,opis){
-        this.ime=ime;
-        this.budget=budget;
-        this.opis=opis;
-        this.container=null;
-        this.cache=null;
-        this.radnici=[];
+export class Sektor {
+    constructor(ime, budget, opis) {
+        this.ime = ime;
+        this.budget = budget;
+        this.opis = opis;
+        this.container = null;
+        this.cache = null;
+        this.radnici = [];
+    }
+
+    crtajSebe(host) {
+        if (!host) {
+            throw new Error("Nema hosta!");
+        }
+    }
+    postaviId(id) {
+        this.id = id;
     }
 }
